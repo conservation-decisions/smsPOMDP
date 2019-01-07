10053e4cdce9e8e4d20b1cad41a81ecdc8750ce9
lvpascal <45868959+lvpascal@users.noreply.github.com>
2019-01-07 15:23:58 +1000

    Update sim.R

--- R/sim.R
+++ R/sim.R
@@ -24,14 +24,14 @@ sim = function(t, o, r, state_prior, Tmax, a, discount=0.95, size = 1){
   legend('bottomleft',legend = 'Extant',  bty = "n", cex = size)
   
   plot2 =plot(sim$df$time, sim$df$action, yaxt='n', 
-              pch = 19, xlab = 'Time (years)',ylab = 'Action ', 
+              pch = 19, xlab = 'Time (years)',ylab = 'nnnnn ', 
               ylim = c(0.9,3.1), xlim = c(-1,Tmax))
   legend('topleft',legend = 'Nothing',  bty = "n", cex = size)
   legend('left',legend = 'Survey',  bty = "n", cex = size)
   legend('bottomleft',legend = 'Manage',  bty = "n", cex = size)
   
   plot3 =plot(sim$df$time, sim$df$obs, yaxt='n',
-              pch = 19, xlab = 'Time (years)',ylab = 'Observation ',
+              pch = 19, xlab = 'Time (years)',ylab = 'nnnnn ',
               ylim = c(0.9,2.1), xlim = c(-1,Tmax) )
   legend('topleft',legend = 'Not seen',  bty = "n", cex = size)
   legend('bottomleft',legend = 'Seen',  bty = "n", cex = size)
