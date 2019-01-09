d60e4a79efe90ce16618dc6e2ef0884aa9c4ffec
lvpascal <luzvalerie@hotmail.fr>
2019-01-09 13:40:28 +1000

    add lines for checking the inputs in rew obs and tr functions

--- R/obs.R
+++ R/obs.R
@@ -1,5 +1,12 @@
 #' @export
 obs = function(p0, pm, d0, d, V, Cm, Cs){
+  #tests the inputs
+  stopifnot(p0>=0,p0<=1) #checks if p0 is a probability
+  stopifnot(pm>=0,pm<=1) #checks if pm is a probability
+  stopifnot(d0>=0,d0<=1) #checks if d0 is a probability
+  stopifnot(d>=0,d<=1) #checks if d is a probability
+  stopifnot(V>=0, Cm >= 0, Cs >= 0) #checks if values and costs are positif
+
   manage = nothing = matrix(c(d0,0,1-d0,1), nrow = 2)
   survey = matrix(c(d,0,1-d,1), nrow = 2)
   o = array(c(manage, survey, nothing), dim = c(2,2,3))
--- R/tr.R
+++ R/tr.R
@@ -1,5 +1,12 @@
 #' @export
 tr = function(p0, pm, d0, d, V, Cm, Cs){
+  #tests the inputs
+  stopifnot(p0>=0,p0<=1) #checks if p0 is a probability
+  stopifnot(pm>=0,pm<=1) #checks if pm is a probability
+  stopifnot(d0>=0,d0<=1) #checks if d0 is a probability
+  stopifnot(d>=0,d<=1) #checks if d is a probability
+  stopifnot(V>=0, Cm >= 0, Cs >= 0) #checks if values and costs are positif
+
   pem = 1-pm
   pen = 1-p0
   manage = matrix(c(pm,0,pem,1), nrow = 2)
