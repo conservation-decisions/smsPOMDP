f41c13dba20b688ed4bc2294d35cf99f3dce41ae
lvpascal <luzvalerie@hotmail.fr>
2019-01-09 16:09:19 +1000

    modifications in sim, so the usage section of doc doesnt exceed 90 characters

--- R/sim.R
+++ R/sim.R
@@ -1,5 +1,5 @@
 #' @export
-sim=function(p0, pm, d0, d, V, Cm, Cs, state_prior, Tmax, a = c('Manage', 'Survey', 'Nothing'), discount = 0.95, size = 1)
+sim=function(p0, pm, d0, d, V, Cm, Cs, s, T, a = c('Manage', 'Survey', 'Nothing'), dics = 0.95, size = 1)
 {
   #checking presence of sarsop package
   list.of.packages <- c("sarsop")
@@ -14,42 +14,42 @@ sim=function(p0, pm, d0, d, V, Cm, Cs, state_prior, Tmax, a = c('Manage', 'Surve
   stopifnot(d0>=0,d0<=1) #checks if d0 is a probability
   stopifnot(d>=0,d<=1) #checks if d is a probability
   stopifnot(V>=0, Cm >= 0, Cs >= 0) #checks if values and costs are positif
-  stopifnot(discount>=0, discount <= 1) #checks if values and costs are positif
-  stopifnot(Tmax >0) #positive horizon
+  stopifnot(dics>=0, dics <= 1) #checks if values and costs are positif
+  stopifnot(T >0) #positive horizon
 
   #buiding the matrices of the problem
   t = TigerTest::tr(p0, pm, d0, d, V, Cm, Cs) #transition matrix
   o = TigerTest::obs(p0, pm, d0, d, V, Cm, Cs)#observation matrix
   r = TigerTest::rew(p0, pm, d0, d, V, Cm, Cs) #reward matrix
 
-  alpha = sarsop::sarsop(t, o, r, discount, state_prior)
+  alpha = sarsop::sarsop(t, o, r, dics, s)
   x0 = 1
   a0 = switch(a, Manage = 1, Survey = 2, Nothing = 3)
-  sim <- sarsop::sim_pomdp(t, o, r, discount, state_prior = state_prior,
-    x0 = x0, a0 = a0, Tmax = Tmax, alpha = alpha)
+  sim <- sarsop::sim_pomdp(t, o, r, dics, s = s,
+    x0 = x0, a0 = a0, T = T, alpha = alpha)
   graphics::par(mfrow = c(4, 1), mai = c(0.7, 0.6, 0.1, 0.1), cex.lab = size)
   plot1 = graphics::plot(sim$df$time, sim$df$state, yaxt = "n", pch = 19,
     xlab = "Time (years)", ylab = "State", ylim = c(0.9,
-      2.1), xlim = c(-2, Tmax), cex = 2)
+      2.1), xlim = c(-2, T), cex = 2)
   graphics::legend("topleft", legend = "Extinct", bty = "n", cex = size)
   graphics::legend("bottomleft", legend = "Extant", bty = "n", cex = size)
   plot2 = graphics::plot(c(0, sim$df$time), c(a0, sim$df$action), yaxt = "n",
     pch = 19, xlab = "Time (years)", ylab = "Action", ylim = c(0.9,
-      3.1), xlim = c(-2, Tmax), cex = 2)
+      3.1), xlim = c(-2, T), cex = 2)
   graphics::legend("topleft", legend = "Nothing", bty = "n", cex = size)
   graphics::legend("left", legend = "Survey", bty = "n", cex = size)
   graphics::legend("bottomleft", legend = "Manage", bty = "n", cex = size)
   plot3 = graphics::plot(sim$df$time, sim$df$obs, yaxt = "n", pch = 19,
     xlab = "Time (years)", ylab = "Observation", ylim = c(0.9,
-      2.1), xlim = c(-2, Tmax), cex = 2)
+      2.1), xlim = c(-2, T), cex = 2)
   graphics::legend("topleft", legend = "Not seen", bty = "n", cex = size)
   graphics::legend("bottomleft", legend = "Seen", bty = "n", cex = size)
   sim$state_posterior = as.data.frame(sim$state_posterior)
   names(sim$state_posterior) = c("extant", "extinct")
-  plot4 = graphics::plot(c(0:(Tmax - 1)), sim$state_posterior$extant,
+  plot4 = graphics::plot(c(0:(T - 1)), sim$state_posterior$extant,
     type = "l", xlab = "Time (years)", ylab = "Probabilities",
-    ylim = c(0, 1), xlim = c(-2, Tmax) )
-  graphics::lines(c(0:(Tmax - 1)), sim$state_posterior$extinct, col = "red",
+    ylim = c(0, 1), xlim = c(-2, T) )
+  graphics::lines(c(0:(T - 1)), sim$state_posterior$extinct, col = "red",
     cex = size)
   graphics::legend("bottomleft", legend = c("Extant", "Extinct"), col = c("black",
     "red"), lty = 1, bty = "n", cex = size)
