ecb25e4f8d7be2a213e4aa34449532e0a59d94f4
lvpascal <luzvalerie@hotmail.fr>
2019-01-11 15:24:51 +1000

    Modify run_application : changed title, parenthesis, added discount factor

--- R/run_application.R
+++ R/run_application.R
@@ -2,23 +2,25 @@
 run_application = function(){
   app <- shiny::shinyApp(
     ui <- shiny::fluidPage(
-      shiny::titlePanel("POMDP solver"),
+      shiny::titlePanel("POMDP problem solver: When to stop managing or surveying cryptic threatened species ?"),
       shiny::sidebarLayout(
-        shiny::sidebarPanel("Species' parameters"
-                     , shiny::numericInput('p0', 'Local probability of persistance (if survey or nothing)', min = 0, max = 1, value = 0.9)
-                     , shiny::numericInput('pm', 'Local probability of persistance if managed', min = 0, max = 1, value = 0.94184)
+        shiny::sidebarPanel("POMDP parameters"
+                     , shiny::numericInput('p0', 'Local probability of persistence (if survey or nothing)', min = 0, max = 1, value = 0.9)
+                     , shiny::numericInput('pm', 'Local probability of persistence (if managed)', min = 0, max = 1, value = 0.94184)
                      , shiny::numericInput('d0', 'Local probability of detection (if managed or nothing)', min = 0, max = 1, value = 0.01)
-                     , shiny::numericInput('d', 'Local probability of detection if surveyed', min = 0, max = 1, value = 0.78193)
+                     , shiny::numericInput('d', 'Local probability of detection (if surveyed)', min = 0, max = 1, value = 0.78193)
                      , shiny::numericInput('V', 'Estimated economic value of the species ($/yr)', value = 175.133)
                      , shiny::numericInput('Cm', 'Estimated cost of managing ($/yr)', value = 18.784)
                      , shiny::numericInput('Cs', 'Estimated cost of surveying ($/yr)', min = 0, max = 1, value = 10.840)
-                     , shiny::numericInput('b', 'Initial belief state (extant)', value = 1)
+                     , shiny::numericInput('disc', 'Discount factor', value = 0.95, max = 1, min = 0)
+                     , helpText('Parameters only for the simulation')
                      , shiny::numericInput('Tmax', 'Duration of simulation', value = 10)
+                     , shiny::numericInput('b', 'Initial belief state (extant)', value = 1)
                      , shiny::selectInput('a0', 'Previous action', choices = c('Manage','Survey','Nothing'), selected = 'Manage')
                      , shiny::actionButton('sim', 'View simulation')
                      , shiny::actionButton('graph', 'View graphical solution')
         )
-        , shiny::mainPanel("Simulation or main solution"
+        , shiny::mainPanel(""
                     , shiny::plotOutput('plot', height = '1000px')
         )
       )
@@ -35,17 +37,18 @@ run_application = function(){
       V = shiny::reactive(input$V)
       Cm = shiny::reactive(input$Cm)
       Cs = shiny::reactive(input$Cs)
+      disc = shiny::reactive(input$disc)
       b = shiny::reactive(input$b)
       state_prior = shiny::reactive({c(b(), 1-b())})
       Tmax = shiny::reactive(input$Tmax)
       a0 = shiny::reactive(input$a0)
 
       shiny::observeEvent(input$sim, {
-        output$plot = shiny::renderPlot({TigerPOMDP::sim(p0(), pm(), d0(), d(), V(), Cm(), Cs(), state_prior(), Tmax(), a0(), disc = 0.95, size = 2)})
+        output$plot = shiny::renderPlot({TigerPOMDP::sim(p0(), pm(), d0(), d(), V(), Cm(), Cs(), state_prior(), Tmax(), a0(), disc(), size = 2)})
       })
 
       shiny::observeEvent(input$graph, {
-        output$plot = shiny::renderPlot({TigerPOMDP::graph(p0(), pm(), d0(), d(), V(), Cm(), Cs(), disc = 0.95, size = 2)})
+        output$plot = shiny::renderPlot({TigerPOMDP::graph(p0(), pm(), d0(), d(), V(), Cm(), Cs(), disc(), size = 2)})
       })
     }
   )
