7e7b00b81ebd92a3b63ce789a959b94f23b7fd4f
lvpascal <luzvalerie@hotmail.fr>
2019-01-25 16:57:14 +1000

    Update run_application

--- R/run_application.R
+++ R/run_application.R
@@ -5,22 +5,24 @@ run_application = function(){
       shiny::titlePanel("POMDP solver: When to stop managing or surveying cryptic threatened species ?"),
       shiny::sidebarLayout(
         shiny::sidebarPanel("POMDP parameters"
-                     , shiny::numericInput('p0', 'Local probability of persistence (if survey or nothing)', min = 0, max = 1, value = 0.9)
-                     , shiny::numericInput('pm', 'Local probability of persistence (if managed)', min = 0, max = 1, value = 0.94184)
-                     , shiny::numericInput('d0', 'Local probability of detection (if managed or nothing)', min = 0, max = 1, value = 0.01)
-                     , shiny::numericInput('d', 'Local probability of detection (if surveyed)', min = 0, max = 1, value = 0.78193)
-                     , shiny::numericInput('V', 'Estimated economic value of the species ($/yr)', value = 175.133)
-                     , shiny::numericInput('Cm', 'Estimated cost of managing ($/yr)', value = 18.784)
-                     , shiny::numericInput('Cs', 'Estimated cost of surveying ($/yr)', min = 0, max = 1, value = 10.840)
-                     , shiny::numericInput('disc', 'Discount factor', value = 0.95, max = 1, min = 0)
-                     , shiny::helpText('Parameters only for the simulation')
-                     , shiny::numericInput('Tmax', 'Duration of simulation', value = 10)
-                     , shiny::numericInput('b', 'Initial belief state (extant)', value = 1)
-                     , shiny::actionButton('sim', 'View simulation')
-                     , shiny::actionButton('graph', 'View graphical solution')
+                            , shiny::numericInput('p0', 'Local probability of persistence (if survey or nothing)', min = 0, max = 1, value = 0.9)
+                            , shiny::numericInput('pm', 'Local probability of persistence (if managed)', min = 0, max = 1, value = 0.94184)
+                            , shiny::numericInput('d0', 'Local probability of detection (if managed or nothing)', min = 0, max = 1, value = 0.01)
+                            , shiny::numericInput('d', 'Local probability of detection (if surveyed)', min = 0, max = 1, value = 0.78193)
+                            , shiny::numericInput('V', 'Estimated economic value of the species ($/yr)', value = 175.133)
+                            , shiny::numericInput('Cm', 'Estimated cost of managing ($/yr)', value = 18.784)
+                            , shiny::numericInput('Cs', 'Estimated cost of surveying ($/yr)', min = 0, max = 1, value = 10.840)
+                            , shiny::numericInput('disc', 'Discount factor', value = 0.95, max = 1, min = 0)
+                            , shiny::helpText('Parameters only for the simulation')
+                            , shiny::numericInput('Tmax', 'Duration of simulation', value = 10)
+                            , shiny::numericInput('b', 'Initial belief state (extant)', value = 1)
+                            , shiny::actionButton('sim', 'View simulation')
+                            , shiny::actionButton('graph', 'View graphical solution')
+                            , shiny::actionButton('past', 'Set past management stream')
         )
         , shiny::mainPanel(""
-                    , shiny::plotOutput('plot', height = '1000px')
+                           , shiny::uiOutput('main')
+
         )
       )
 
@@ -41,13 +43,97 @@ run_application = function(){
       state_prior = shiny::reactive({c(b(), 1-b())})
       Tmax = shiny::reactive(input$Tmax)
 
+      #different scenarios depending on the users choice
+      #launch a simulation
       shiny::observeEvent(input$sim, {
         output$plot = shiny::renderPlot({TigerPOMDP::sim(p0(), pm(), d0(), d(), V(), Cm(), Cs(), state_prior(), Tmax(), disc(), size = 2)})
+        output$main = renderUI({
+          shiny::plotOutput('plot', height = '1000px')
+        })
       })
 
+      #see decision graph
       shiny::observeEvent(input$graph, {
         output$plot = shiny::renderPlot({TigerPOMDP::graph(p0(), pm(), d0(), d(), V(), Cm(), Cs(), c(1,0), disc(), size = 2)})
+        output$main = renderUI({
+          shiny::plotOutput('plot', height = '1000px')
+        })
       })
+
+      #################################################################################
+      #give a set of past actions and observations and see the best following strategy#
+      #################################################################################
+
+      #correponding panel
+      shiny::observeEvent(input$past, {
+        output$main = renderUI({
+          shiny::tagList(
+            shiny::column(width = 2
+                          , shiny::numericInput('length_past', "Number of years for past management", value = 1, min = 1)
+                          , shiny::actionButton('submit_length_past', 'Submit')
+                          , shiny::uiOutput('past_control')
+            )
+            , shiny::column(width = 9
+                            , shiny::plotOutput('past_plot')
+                            , shiny::actionButton('next_policy', 'Next policy')
+                            , shiny::plotOutput('next_policy_plot')
+            )
+
+
+          )
+        })
+      })
+
+      #set past management stream
+      observeEvent(input$submit_length_past,{
+        output$past_control = shiny::renderUI({
+          shiny::tagList(
+            shiny::h5(paste0('Year ', 1))
+            , shiny::selectInput(inputId = paste0("past_action_", 1)
+                                 ,label = 'Action'
+                                 ,choices = c('Manage', 'Survey', 'Nothing')
+                                 , selected = 'Manage')
+            , shiny::selectInput(inputId = paste0("past_obs_", 1)
+                                 ,label = 'Observation'
+                                 ,choices = c('Seen', 'Not_seen')
+                                 , selected = 'Seen')
+            , shiny::actionButton(inputId = paste0("submit_couple_", 1)
+                                  , label = 'Submit couple')
+            , conditionalPanel('input.length_past > 1',
+                               lapply(c(2:input$length_past),
+                                      function(i){
+                                        shiny::conditionalPanel(paste0('input.submit_couple_', i-1)
+                                                                , shiny::h5(paste0('Year ', i))
+                                                                , shiny::selectInput(inputId = paste0("past_action_", i)
+                                                                                     ,label = 'Action'
+                                                                                     ,choices = c('Manage', 'Survey', 'Nothing')
+                                                                                     , selected = 'Manage')
+                                                                , shiny::selectInput(inputId = paste0("past_obs_", i)
+                                                                                     ,label = 'Observation'
+                                                                                     ,choices = c('Seen', 'Not_seen')
+                                                                                     , selected = 'Seen')
+                                                                , shiny::actionButton(inputId = paste0("submit_couple_", i)
+                                                                                      , label = 'Submit couple')
+                                        )
+
+                                      })
+            )
+          )
+        })
+      })
+
+      p_a = shiny::reactive(TigerPOMDP::past_actions(input)) #past actions
+      p_o = shiny::reactive(TigerPOMDP::past_obs(input)) #past observations
+      current_belief = shiny::reactive(TigerPOMDP::compute_belief(p0(), pm(), d0(), d(), V(), Cm(), Cs(),c(1,0), p_a(), p_o(), disc()))
+      #
+      observeEvent(input$submit_couple_1, {
+        output$past_plot = shiny::renderPlot(TigerPOMDP::plot_stream(p0(), pm(), d0(), d(), V(), Cm(), Cs(),c(1,0), p_a(), p_o(), disc()))
+      })
+      observeEvent(input$next_policy, {
+        output$next_policy_plot = shiny::renderPlot({TigerPOMDP::graph(p0(), pm(), d0(), d(), V(), Cm(), Cs(), current_belief(), disc())})
+      })
+
+
     }
   )
   shiny::runApp(app)
