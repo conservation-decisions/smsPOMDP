ecae7e3d94851aba9a50cde99920dab6b0f4404b
conservation-decisions <45868959+conservation-decisions@users.noreply.github.com>
2019-03-27 11:46:08 +1000

    Update sim.R

--- R/sim.R
+++ R/sim.R
@@ -10,103 +10,103 @@ sim=function(p0, pm, d0, d, V, Cm, Cs, state_prior, T, disc = 0.95, size = 1)
   stopifnot(V>=0, Cm >= 0, Cs >= 0) #checks if values and costs are positif
   stopifnot(disc>=0, disc <= 1) #checks if values and costs are positif
   stopifnot(T >0) #positive horizon
+#Compute matrices
+t = smsPOMDP::tr(p0, pm, d0, d, V, Cm, Cs)
+o = smsPOMDP::obs(p0, pm, d0, d, V, Cm, Cs)
+r = smsPOMDP::rew(p0, pm, d0, d, V, Cm, Cs)
 
-  #buiding the matrices of the problem
-  t = smsPOMDP::tr(p0, pm, d0, d, V, Cm, Cs) #transition matrix
-  o = smsPOMDP::obs(p0, pm, d0, d, V, Cm, Cs)#observation matrix
-  r = smsPOMDP::rew(p0, pm, d0, d, V, Cm, Cs) #reward matrix
+#solve POMDP
+alpha = sarsop::sarsop(t, o, r, disc, state_prior)
+log_dir = tempdir()
+id <- digest::digest(match.call())
+infile <- paste0(log_dir, "/", id, ".pomdpx")
+outfile <- paste0(log_dir, "/", id, ".policyx")
+stdout <- paste0(log_dir, "/", id, ".log")
+sarsop::write_pomdpx(t, o, r, disc, state_prior, file = infile)
+status <- sarsop::pomdpsol(infile, outfile, stdout = stdout)
+policy <- sarsop::read_policyx(file = outfile)
 
-  alpha = sarsop::sarsop(t, o, r, disc, state_prior)
-
-  log_dir = tempdir()
-  id <- digest::digest(match.call())
-  infile <- paste0(log_dir, "/", id, ".pomdpx")
-  outfile <- paste0(log_dir, "/", id, ".policyx")
-  stdout <- paste0(log_dir, "/", id, ".log")
-  simout <- paste0(log_dir, "/", id, ".sim")
-
-  sarsop::write_pomdpx(t, o, r, disc, state_prior, file = infile)
-  status <- sarsop::pomdpsol(infile, outfile, stdout = stdout)
-  g = sarsop::pomdpsim(infile, outfile, simout, steps = T,
-                       simulations = 1, stdout = stdout, spinner = TRUE)
-
-  g = readChar(simout, file.info(simout)$size)
-  g = strsplit(g, split = '\r\n')
-  g = unlist(g)
-  i = min(which(grepl('R', g)))+1
-  j = which(grepl('terminated', g))-1
-  st = function(c){
-    char = c
-    char = stringr::str_replace(char, "Y   :\\(", '')
-    char = stringr::str_replace(char, 'O   :\\(', '')
-    char = stringr::str_replace(char, 'ML Y:\\(', '')
-    char = stringr::str_replace(char, 'A   :\\(', '')
-    char = stringr::str_replace(char, 'R   :', '')
-    char = stringr::str_replace(char, '\\)', '')
-    return(char)
+#initialise
+#randomize real initial state
+set.seed( as.integer((as.double(Sys.time())*1000+Sys.getpid()) %% 2^31) )
+rand = stats::runif(1)
+if (rand <= state_prior[1]){
+  real_state = 1
+} else {
+  real_state = 2
+}
+#compute best action given belief state
+output <- smsPOMDP::Interp_policy(state_prior,policy$vectors,policy$action)
+actions = output[[2]][1]
+#random observation
+set.seed( as.integer((as.double(Sys.time())*2000+Sys.getpid()) %% 2^31) )
+rand = stats::runif(1)
+if (rand <= o[real_state,1, actions]){
+  observations = 1
+} else {
+  observations = 2
+}
+state_posterior = matrix(state_prior, ncol = 2)
+for (i in c(1:(T - 1))){
+  a1 = actions[i]
+  o1 = observations[i]
+  #compute next belief state
+  s_p <- smsPOMDP::update_belief(state_posterior[i, ], 
+                                 t, o, o1, a1)
+  state_posterior = rbind(state_posterior, s_p)
+  set.seed( as.integer((as.double(Sys.time())*i*1000+Sys.getpid()) %% 2^31) )
+  rand = stats::runif(1)
+  if (rand < t[real_state[i],1,a1]){
+    r_s = 1
+    real_state = c(real_state,1)
+  } else {
+    r_s = 2
+    real_state = c(real_state,2)
   }
-  sim = unlist(lapply(g[i:j], st))
-  sim=matrix(sim, ncol = 5, byrow = T)
-  colnames(sim) = c('Y', 'O', 'ML Y', 'A', 'R')
-
-  init = unlist(lapply(g[2:(i-1)], st))
-
-  state = c(init[1], sim[,1])
-  conv_state = function(asdf){
-    switch (asdf, a1 = 1, a2 = 2)
+  #compute best action given belief state
+  output <- smsPOMDP::Interp_policy(s_p,policy$vectors,policy$action)
+  actions = c(actions, output[[2]][1])
+  #random observation
+  set.seed( as.integer((as.double(Sys.time())*i*21000+Sys.getpid()) %% 2^31) )
+  rand = stats::runif(1)
+  if (rand <= o[r_s,1, actions[i+1]]){
+    observations = c(observations,1)
+  } else {
+    observations = c(observations,2)
   }
-  state = unlist(lapply(state, conv_state))
-
-  action = c(init[3], sim[,4])
-  conv_action = function(asdf){
-    switch (asdf, a1 = 1, a2 = 2, a3 = 3)
-  }
-  action = unlist(lapply(action, conv_action))
-
-  obs = c(sim[,2])
-  conv_obs = function(asdf){
-    switch (asdf, o0 = 1, o1 = 2)
-  }
-  obs = unlist(lapply(obs, conv_obs))
-
-  #calcul of belief states, extant
-  state_posterior = matrix(state_prior, ncol = 2)
-
-  for (i in c(1:(T-1))){
-    a1 = action[i]
-    o1 = obs[i]
-    s_p <- smsPOMDP::update_belief(state_posterior[i,], t, o, o1, a1)
-    state_posterior = rbind(state_posterior,s_p)
-  }
-
-  graphics::par(mfrow = c(4, 1), mai = c(0.7, 0.6, 0.1, 0.1), cex.lab = size)
-
-  #States
-  plot1 = graphics::plot(c(0:(T-1)), state, yaxt = "n", pch = 19,
-    xlab = "Time (years)", ylab = "State", ylim = c(0.9,
-      2.1), xlim = c(-2, T), cex = 2)
-  graphics::legend("topleft", legend = "Extinct", bty = "n", cex = size)
-  graphics::legend("bottomleft", legend = "Extant", bty = "n", cex = size)
-  #Actions
-  plot2 = graphics::plot(c(0:(T-1)),action, yaxt = "n",
-    pch = 19, xlab = "Time (years)", ylab = "Action", ylim = c(0.9,
-      3.1), xlim = c(-2, T), cex = 2)
-  graphics::legend("topleft", legend = "Nothing", bty = "n", cex = size)
-  graphics::legend("left", legend = "Survey", bty = "n", cex = size)
-  graphics::legend("bottomleft", legend = "Manage", bty = "n", cex = size)
-  #Observation
-  plot3 = graphics::plot(c(1:(T-1)), obs, yaxt = "n", pch = 19,
-    xlab = "Time (years)", ylab = "Observation", ylim = c(0.9,
-      2.1), xlim = c(-2, T), cex = 2)
-  graphics::legend("topleft", legend = "Not seen", bty = "n", cex = size)
-  graphics::legend("bottomleft", legend = "Seen", bty = "n", cex = size)
+}
 
 
-  plot4 = graphics::plot(c(0:(T - 1)), state_posterior[,1],
-    type = "l", xlab = "Time (years)", ylab = "Probabilities",
-    ylim = c(0, 1), xlim = c(-2, T) )
-  graphics::lines(c(0:(T - 1)), state_posterior[,2], col = "red",
-    cex = size)
-  graphics::legend("bottomleft", legend = c("Extant", "Extinct"), col = c("black",
-    "red"), lty = 1, bty = "n", cex = size)
+graphics::par(mfrow = c(4, 1), mai = c(0.7, 0.6, 0.1, 0.1), 
+              cex.lab = size)
+plot1 = graphics::plot(c(0:(T - 1)), real_state, yaxt = "n", 
+                       pch = 19, xlab = "Time (years)", ylab = "State", ylim = c(0.9, 
+                                                                                 2.1), xlim = c(-2, T), cex = 2)
+graphics::legend("topleft", legend = "Extinct", bty = "n", 
+                 cex = size)
+graphics::legend("bottomleft", legend = "Extant", bty = "n", 
+                 cex = size)
+plot2 = graphics::plot(c(0:(T - 1)), actions, yaxt = "n", 
+                       pch = 19, xlab = "Time (years)", ylab = "Action", ylim = c(0.9, 
+                                                                                  3.1), xlim = c(-2, T), cex = 2)
+graphics::legend("topleft", legend = "Nothing", bty = "n", 
+                 cex = size)
+graphics::legend("left", legend = "Survey", bty = "n", cex = size)
+graphics::legend("bottomleft", legend = "Manage", bty = "n", 
+                 cex = size)
+plot3 = graphics::plot(c(1:(T)), observations, yaxt = "n", pch = 19, 
+                       xlab = "Time (years)", ylab = "Observation", ylim = c(0.9, 
+                                                                             2.1), xlim = c(-2, T), cex = 2)
+graphics::legend("topleft", legend = "Not seen", bty = "n", 
+                 cex = size)
+graphics::legend("bottomleft", legend = "Seen", bty = "n", 
+                 cex = size)
+plot4 = graphics::plot(c(0:(T - 1)), state_posterior[, 1], 
+                       type = "l", xlab = "Time (years)", ylab = "Probabilities", 
+                       ylim = c(0, 1), xlim = c(-2, T))
+graphics::lines(c(0:(T - 1)), state_posterior[, 2], col = "red", 
+                cex = size)
+graphics::legend("bottomleft", legend = c("Extant", "Extinct"), 
+                 col = c("black", "red"), lty = 1, bty = "n", cex = size)
+
 }
