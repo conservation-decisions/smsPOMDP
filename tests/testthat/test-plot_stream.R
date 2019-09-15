52bf662b202ee0fd45d6dd9c52ed3c572e875447
unknown <luz.pascal96@gmail.com>
2019-09-15 22:30:51 +0200

    test that vdiffr

--- tests/testthat/test-plot_stream.R
+++ tests/testthat/test-plot_stream.R
@@ -1,9 +1,22 @@
 library("ggplot2")
 
 test_that("plots have known output", {
-  disp_hist_base <- function() hist(mtcars$disp)
-  expect_doppelganger("disp-histogram-base", disp_hist_base)
+  #values for Sumatran tigers
+  pen = 0.1
+  p0 = 1-pen
+  pem = 0.05816
+  pm = 1 - pem
+  V = 175.133
+  Cm = 18.784
+  Cs = 10.840
+  d0 = 0.01
+  d = 0.78193
   
-  disp_hist_ggplot <- ggplot(mtcars, aes(disp)) + geom_histogram()
-  expect_doppelganger("disp-histogram-ggplot", disp_hist_ggplot)
+  s = c(1,0)
+  act = c('Manage', 'Manage', 'Survey','Stop')
+  obs = c('Not_seen','Not_seen','Not_seen','Seen')
+  disp_plot_stream = function () smsPOMDP::plot_stream(p0, pm, d0, d, V, Cm, Cs, s, act, obs, disc = 0.95, size = 1)
+  
+  vdiffr::expect_doppelganger("disp-plot-stream-base", disp_plot_stream)
+  
 })
